11d11ebd92fe3ab909c2fec5966a7694c19d5bde
romarios1987 <romios047@gmail.com>
2021-09-26 20:49:06 +0300

    add toolbar & update drawer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>Hello</div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item v-for="(link, i) in links" :key="i" :to="link.url">
+            <v-list-item-icon>
+              <v-icon v-text="link.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="link.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="blue darken-4" dark>
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+        class="hidden-md-and-up"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>AdvertApp</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text v-for="(link, i) in links" :key="i" :to="link.url">
+          <v-icon left v-text="link.icon"></v-icon>
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -9,7 +42,14 @@ export default {
   components: {},
 
   data: () => ({
-    //
+    drawer: null,
+    links: [
+      { title: "Login", icon: "mdi-lock", url: "/login" },
+      { title: "Registration", icon: "mdi-face", url: "/register" },
+      { title: "Orders", icon: "mdi-bookmark-outline", url: "/orders" },
+      { title: "New advert", icon: "mdi-note-plus", url: "/new" },
+      { title: "My adverts", icon: "mdi-format-list-bulleted", url: "/list" },
+    ],
   }),
 };
 </script>
